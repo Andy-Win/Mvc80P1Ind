d78ae82a648471d5dc1167b1280b4ce8d70f1610
Andy Wang <v-wajian@microsoft.com>
2023-04-17 08:51:08 +0000

    Add project files.

---
+++ Mvc80P1Ind/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc80P1Ind.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
